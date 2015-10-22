97042f1941130c076161ab52c3928c64719ed659
tryangul <ryan.broughan@gmail.com>
2015-10-22 16:08:04 -0700

    Voting screen UI handles voting and winners

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -2,7 +2,7 @@ import React from "react/addons";
 import Voting from "../../src/components/Voting";
 import { expect } from "chai";
 
-const {renderIntoDocument, scryRenderedDOMComponentsWithTag} = React.addons.TestUtils;
+const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} = React.addons.TestUtils;
 
 describe("Voting", () => {
 
@@ -15,6 +15,52 @@ describe("Voting", () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Trainspotting');
     expect(buttons[1].textContent).to.equal('28 Days Later');
+  });
+
+  it("invokes callback when a button is clicked", () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={['Trainspotting', '28 Days Later']}
+              vote={vote}/>
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
+  });
+
+  it("disables buttons when user has voted", () => {
+    const component = renderIntoDocument(
+      <Voting pair={['Trainspotting', '28 Days Later']}
+              hasVoted="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons[0].textContent).to.contain('Voted');
+  });
+
+  it("adds a label to the voted entry", () => {
+    const component = renderIntoDocument(
+      <Voting pair={['Trainspotting', '28 Days Later']}
+              hasVoted="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons[0].textContent).to.contain('Voted');
+  });
+
+  it("renders just the winner when there is one", () => {
+    const component = renderIntoDocument(
+      <Voting winner="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    expect(buttons.length).to.equal(0);
+
+    const winner = React.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
   });
 
 });
