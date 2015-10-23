39ac4aa08e44096136229a9f8653088ea2288255
tryangul <ryan.broughan@gmail.com>
2015-10-22 20:05:39 -0700

    set up reducer and create redux store

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Router, { Route } from "react-router";
+import { createStore } from "redux";
+import reducer from "./reducer";
+import App from "./components/App";
 import Voting from "./components/Voting";
+import Results from "./components/Results";
 require('./style.css');
 
-const pair = ['Trainspotting', '28 Days Later'];
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
+
+
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
+
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Router>{routes}</Router>,
   document.getElementById('app')
 );
